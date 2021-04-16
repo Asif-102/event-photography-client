bfbb2a3494e8495e68fee1e77796ed66f824a91c
Asif2535 <aiqbal517@gmail.com>
2021-04-16 22:06:42 +0600

    added review from database

--- src/components/Home/Testimonials/Testimonials.js
+++ src/components/Home/Testimonials/Testimonials.js
@@ -1,39 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import user from '../../../images/user.svg';
 import Testimonial from './Testimonial/Testimonial';
 
-const testimonials = [
-    {
-        id:1,
-        img:user,
-        name:'karim',
-        designation: 'CEO, abc',
-        review : 'very dedicated and passionate'
-    },
-    {
-        id:2,
-        img:user,
-        name:'Rafiq',
-        designation: 'MD, kdf',
-        review : 'very good'
-    },
-    {
-        id:3,
-        img:user,
-        name:'Jamal',
-        designation: 'Manager, bata',
-        review : 'awesome'
-    }
-]
-
 const Testimonials = () => {
+
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/reviews')
+        .then(res => res.json())
+        .then(data => setReviews(data))
+    },[])
+
     return (
         <section className="container py-5 mb-5">
             <h1 className='text-center font-weight-bold py-4 mb-4'>Clients Feedback</h1>
             <div className="row justify-content-between pt-4">
                 {
-                    testimonials.map(testimonial => <Testimonial key={testimonial.id}
-                    testimonial={testimonial}></Testimonial>)
+                    reviews.map(revew => <Testimonial key={revew._id}
+                        revew={revew}></Testimonial>)
                 }
             </div>
         </section>
