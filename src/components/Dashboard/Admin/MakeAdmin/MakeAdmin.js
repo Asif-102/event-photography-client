8870f172e107c89f9d198e65b7bfc4a3b380912f
Asif2535 <aiqbal517@gmail.com>
2021-04-16 16:50:10 +0600

    configure with added admin api

--- src/components/Dashboard/Admin/MakeAdmin/MakeAdmin.js
+++ src/components/Dashboard/Admin/MakeAdmin/MakeAdmin.js
@@ -3,12 +3,26 @@ import { useForm } from 'react-hook-form';
 
 const MakeAdmin = () => {
     const {register, handleSubmit, errors, reset} = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        const adminEmail = {
+            email:data.email
+        }
+        const url = `http://localhost:5000/addAdmin`;
+
+        fetch(url,{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(adminEmail)
+        })
+        .then(res => console.log('server side response', res))
+    }
     return (
         <div>
             <form onSubmit={handleSubmit(onSubmit)} style={{maxWidth:'570px'}}>
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" placeholder="john@gmail.com" ref={register({required:true})} className="form-control"/>
+                <input type="email" id="email" name="email" placeholder="example@gmail.com" ref={register({required:true})} className="form-control"/>
                 {errors.email && <span className="text-danger"><small>Email cannot be empty</small></span>}
                 <br></br>
                 <input type="submit" className="btn btn-primary px-3 py-2"value="Submit"/>
