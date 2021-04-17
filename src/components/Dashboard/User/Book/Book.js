de67df179535c8fd27b74b40ae27e4f7fbe7ed57
Asif2535 <aiqbal517@gmail.com>
2021-04-17 18:06:16 +0600

    finished booking section with payment gateway

--- src/components/Dashboard/User/Book/Book.js
+++ src/components/Dashboard/User/Book/Book.js
@@ -11,45 +11,71 @@ const Book = () => {
     const { name, email } = loggedInUser;
 
     const [serviceFind, setServiceFind] = useState({});
-    const { title, price } = serviceFind;
+    const { title, price, image } = serviceFind;
+
+    const [visible, setVisible] = useState([])
 
     const [bookService, setBookService] = useState(null);
 
     useEffect(() => {
         fetch(`http://localhost:5000/getService?_id=${id}`)
             .then(res => res.json())
-            .then(data => setServiceFind(data[0]))
+            .then(data => {
+                setServiceFind(data[0])
+                setVisible(data)
+            })
     }, [id])
 
-    const { register, handleSubmit, errors, reset } = useForm();
-    const onSubmit = data => {
-        setBookService(data);
-    }
+    const { register, reset } = useForm();
 
-    const handlePaymentSuccess = paymentId =>{
-        console.log(paymentId);
+    const handlePaymentSuccess = paymentId => {
+        const bookingData = {
+            userName:name,
+            userEmail:email,
+            serviceTitle:title,
+            serviceImg:image,
+            serviceCost:price,
+            payId:paymentId,
+            bookingState:'Pending'
+        }
+        const url  = `http://localhost:5000/addBooking`;
+
+        fetch(url,{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(bookingData)
+        })
+        .then(response =>{
+            if(response.ok){
+                alert('booking successfull');
+            }
+        })
     }
 
     return (
-        <main className="row">
-            <div style={{display: bookService ? 'none': 'block'}} className="col-6">
-                <form style={{ maxWidth: '570px' }} onSubmit={handleSubmit(onSubmit)}>
-
-                    <input type="text" name="name" placeholder="Your name/ Company name" className="form-control" defaultValue={name} ref={register({ required: true })} />
-                    {errors.name && <span className="text-danger"><small>Name is required</small></span>}
-
-                    <input type="email" name="email" placeholder="Your email address" className="form-control" defaultValue={email} ref={register({ required: true })} />
-                    {errors.email && <span className="text-danger"><small>Email is required</small></span>}
-
-                    <input type="text" name="service" placeholder="Service Title" className="form-control" defaultValue={title} ref={register({ required: true })} />
-                    {errors.service && <span className="text-danger"><small>Service is required</small></span>}<br></br>
-
-                    <h5>Your Service charged will be <span className="text-primary">${price}</span></h5> <br />
-                    <input type="submit" className="btn btn-primary px-4 py-2" value="Submit" />
-                </form>
+        <main>
+            <div style={{display: visible.length == 0 ? 'block':'none'}}>
+                <h2>Please Select a Service from Service Section for booking</h2>
             </div>
-            <div style={{display: bookService ? 'block': 'none'}} className="col-6">
-                <ProcessPayment handlePayment={handlePaymentSuccess}></ProcessPayment>
+            <div style={{display: visible.length == 0 ? 'none':'block'}}>
+                <div style={{ display: bookService ? 'none' : 'block' }} className="col-6">
+                    <form style={{ maxWidth: '570px' }}>
+
+                        <input type="text" name="name" placeholder="Your name/ Company name" className="form-control" defaultValue={name} ref={register({ required: true })} disabled/>
+
+                        <input type="email" name="email" placeholder="Your email address" className="form-control" defaultValue={email} ref={register({ required: true })} disabled/>
+                        
+                        <input type="text" name="service" placeholder="Service Title" className="form-control" defaultValue={title} ref={register({ required: true })} disabled/>
+                        <br></br>
+
+                        <h5>Your Service charged will be <span className="text-primary">${price}</span></h5> <br />
+                    </form>
+                </div>
+                <div className="col-6">
+                    <ProcessPayment handlePayment={handlePaymentSuccess}></ProcessPayment>
+                </div>
             </div>
         </main>
     );
